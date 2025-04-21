99580800ab81866e71807a0b1a80cfb33091a4f6
HarrisonTCodes <htillbackup@gmail.com>
2025-04-21 16:00:48 +0100

    feat: changed initial protein visuals

--- src/ursina_proteins/protein.py
+++ src/ursina_proteins/protein.py
@@ -1,5 +1,5 @@
 from Bio.PDB import PDBParser
-from ursina import Entity, Mesh
+from ursina import Entity, Mesh, color
 
 
 class Protein(Entity):
@@ -8,6 +8,11 @@ class Protein(Entity):
         structure = parser.get_structure("protein", pdb_filepath)
 
         atoms = [atom.coord for atom in structure.get_atoms()]
-        model = Mesh(mode="point", vertices=atoms)
+        model = Mesh(
+            mode="point",
+            vertices=atoms,
+            colors=[color.random_color() for _ in atoms],
+            thickness=0.5,
+        )
 
         super().__init__(model=model, *args, **kwargs)
