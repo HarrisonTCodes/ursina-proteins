9cd6c0c5f2da2a3417cccb00ea3eb8cc587aa165
HarrisonTCodes <htillbackup@gmail.com>
2025-04-25 17:19:33 +0100

    refactor: moved atoms and chains computations into standalone functions

--- src/demo.py
+++ src/demo.py
@@ -5,7 +5,7 @@ from ursina_proteins.protein import Protein
 
 def main():
     pdb_filepath = input("Enter PDB file path: ")
-    app = Ursina()
+    app = Ursina(borderless=False)
 
     Protein(pdb_filepath)
 
--- src/ursina_proteins/protein.py
+++ src/ursina_proteins/protein.py
@@ -24,58 +24,74 @@ class Protein:
         atoms_thickness: float = 0.2,
         chains_thickness: float = 4,
         element_color_map: dict[str, Color] = dict(),
-        chain_smoothness: float = 3,
+        chains_smoothness: float = 3,
         *args,
         **kwargs,
     ):
         parser = PDBParser()
-        structure = parser.get_structure("protein", pdb_filepath)
-        structure_centre_of_mass = structure.center_of_mass()
+        self.structure = parser.get_structure("protein", pdb_filepath)
+        structure_center_of_mass = self.structure.center_of_mass()
 
-        # Atoms
-        atoms_mesh = Mesh(
+        self.atoms_entity = Entity(
+            model=self.compute_atoms_mesh(atoms_thickness, element_color_map),
+            origin=structure_center_of_mass,
+            *args,
+            **kwargs,
+        )
+
+        self.chains_entity = Entity(
+            model=self.compute_chains_mesh(chains_thickness, chains_smoothness),
+            origin=structure_center_of_mass,
+            *args,
+            **kwargs,
+        )
+
+    def compute_atoms_mesh(
+        self, thickness: float, element_color_map: dict[str, Color]
+    ) -> Mesh:
+        return Mesh(
             mode="point",
-            vertices=[atom.coord for atom in structure.get_atoms()],
+            vertices=[atom.coord for atom in self.structure.get_atoms()],
             colors=[
                 element_color_map.get(
                     atom.element,
                     Protein.ELEMENT_COLORS.get(atom.element, color.rgb(1, 0.7, 0.8)),
                 )
-                for atom in structure.get_atoms()
+                for atom in self.structure.get_atoms()
             ],
-            thickness=atoms_thickness,
-        )
-        self.atoms_entity = Entity(
-            model=atoms_mesh, origin=structure_centre_of_mass, *args, **kwargs
+            thickness=thickness,
         )
 
-        # Chains
-        chains_coords = []
-        chains_colors = []
-        chains_segments = []
-        for chain in structure.get_chains():
-            segment_start = len(chains_coords)
+    def compute_chains_mesh(self, thickness: float, smoothness: float) -> Mesh:
+        coords = []
+        colors = []
+        segments = []
+
+        for chain in self.structure.get_chains():
+            segment_start = len(coords)
 
             # Coords (vertices)
             carbon_alpha_coords = [
                 atom.coord for atom in chain.get_atoms() if atom.get_id() == "CA"
             ]
+
             # Get spline function for each axis
             x, y, z = zip(*carbon_alpha_coords)
             spline_x = make_splrep(range(len(x)), x, s=0)
             spline_y = make_splrep(range(len(y)), y, s=0)
             spline_z = make_splrep(range(len(z)), z, s=0)
+
             # Calculate splined coordinates
             step_values = np.linspace(
                 0,
                 len(carbon_alpha_coords) - 1,
-                round(len(carbon_alpha_coords) * chain_smoothness),
+                round(len(carbon_alpha_coords) * smoothness),
             )
             helix_x = splev(step_values, spline_x)
             helix_y = splev(step_values, spline_y)
             helix_z = splev(step_values, spline_z)
-            chain_coords = list(zip(helix_x, helix_y, helix_z))
-            chains_coords.extend(chain_coords)
+            helix_coords = list(zip(helix_x, helix_y, helix_z))
+            coords.extend(helix_coords)
 
             # Colors
             hash_value = int(md5(chain.get_id().encode("utf-8")).hexdigest(), 16)
@@ -83,23 +99,20 @@ class Protein:
             g = (hash_value >> 8) & 0xFF
             b = hash_value & 0xFF
             chain_color = color.rgb(r / 255, g / 255, b / 255)
-            chains_colors.extend([chain_color for _ in chain_coords])
+            colors.extend([chain_color for _ in helix_coords])
 
             # Segments (triangles)
-            chains_segments.extend(
+            segments.extend(
                 [
                     (i, i + 1)
-                    for i in range(segment_start, segment_start + len(chain_coords) - 1)
+                    for i in range(segment_start, segment_start + len(helix_coords) - 1)
                 ]
             )
 
-        chains_mesh = Mesh(
+        return Mesh(
             mode="line",
-            vertices=chains_coords,
-            colors=chains_colors,
-            triangles=chains_segments,
-            thickness=chains_thickness,
-        )
-        self.chains_entity = Entity(
-            model=chains_mesh, origin=structure_centre_of_mass, *args, **kwargs
+            vertices=coords,
+            colors=colors,
+            triangles=segments,
+            thickness=thickness,
         )
