98cd50b669bc620f0edf02fbe4af7e0ac8c09772
HarrisonTCodes <htillbackup@gmail.com>
2025-06-30 17:33:08 +0100

    feat: added toggle atoms button to demo

--- src/demo.py
+++ src/demo.py
@@ -1,6 +1,6 @@
 from os import path
 
-from ursina import DirectionalLight, EditorCamera, Ursina
+from ursina import Button, DirectionalLight, EditorCamera, Ursina, Vec2, window
 from ursina.shaders import lit_with_shadows_shader
 
 from ursina_proteins.protein import Protein
@@ -16,6 +16,15 @@ def main():
     )
     light = DirectionalLight(position=(1000, 1000, 1000))
     light.look_at(protein.atoms_entity)
+
+    toggle_atoms_button = Button(
+        text="Toggle atoms visibility",
+        on_click=lambda: protein.atoms_entity.disable()
+        if protein.atoms_entity.enabled
+        else protein.atoms_entity.enable(),
+        scale=(0.3, 0.05),
+        position=window.top_left + Vec2(0.16, -0.04),
+    )
 
     EditorCamera()
     app.run()
