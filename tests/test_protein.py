2474af11166ea025c54bb500e679ced04b2e9b45
HarrisonTCodes <htillbackup@gmail.com>
2025-11-19 19:26:24 +0000

    ci: added mmCIF processing test

--- tests/test_protein.py
+++ tests/test_protein.py
@@ -8,3 +8,7 @@ class TestProteinInit(unittest.TestCase):
     def test_simple_pdb(self):
         base_dir = path.dirname(__file__)
         Protein(path.join(base_dir, "..", "assets", "insulin.pdb"))
+
+    def test_simple_cif(self):
+        base_dir = path.dirname(__file__)
+        Protein(path.join(base_dir, "..", "assets", "insulin.cif"), legacy_pdb=False)
